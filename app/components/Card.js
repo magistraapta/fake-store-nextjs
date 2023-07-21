2eec0ae64b6acbf0f2e695dfae82998b369cda3b
Magistra <magistraagis@gmail.com>
2023-07-21 16:47:32 +0800

    initial commit

---
+++ app/components/Card.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+export default async function Card({...props}) {
+    const {key, title, price, category, image} = props;
+  return (
+    <div className='border '>
+        <div className=' h-[300px] rounded-lg flex justify-center items-center'>
+            <img className='max-h-[200px] max-w-[200px]' src={image} alt="product image" />
+        </div>
+        <div className='my-5 mx-4'>
+            <h1 className='text-xl font-bold'>{title}</h1>
+            <p className=' text-slate-400'>{category}</p>
+            <div className='mt-4'>
+                
+                <h5 className=' text-lg font-semibold'>${price}</h5>
+            </div>
+        </div>
+        
+        
+    </div>
+  )
+}
---
+++ app/product/page.js
@@ -0,0 +1,31 @@
+import React from 'react'
+import Card from "../components/Card";
+import Link from 'next/link';
+export default async function page() {
+const getData = await fetch('https://fakestoreapi.com/products');
+  const products = await getData.json();
+  return (
+    <div className='mx-[50px]'>
+        <div className=''>
+        <div className="grid grid-cols-3 gap-2">
+          {products.map((product) => {
+            return <div key={product.id}>
+                <Link href={`product/${product.id}`}>
+                <Card 
+                    title={product.title} 
+                    image={product.image} 
+                    price={product.price} 
+                    category={product.category}
+                />
+                </Link>
+            
+            </div>
+            
+            
+          })}
+        </div>
+        
+      </div>
+    </div>
+  )
+}

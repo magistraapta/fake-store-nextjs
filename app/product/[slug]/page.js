8b88e30db4eafc406edc938ad48febc8a1ab77af
Magistra <magistraagis@gmail.com>
2023-07-21 17:23:17 +0800

    fix detail page

--- app/product/[slug]/page.js
+++ app/product/[slug]/page.js
@@ -1,29 +1,32 @@
 import React from 'react'
-
+import Navbar from '@/app/components/Navbar'
 export default async function page({params}) {
-    // const getSingle = await fetch(`https://fakestoreapi.com/products/${params}`)
-    // const product = await getSingle.json()
+    const id = params.id
+    const getSingle = await fetch(`https://fakestoreapi.com/products/${id}`)
+    const product = await getSingle.json()
   return (
     <div>
-        <h1>params :{params}</h1>
-    </div>
-    // <div className='mx-[50px] my-[50px] flex items-center'>
-    //     <div className='flex justify-center items-center w-6/12'>
-    //         <img className=' h-[500px]'  src={product.image} alt="product image" />
-    //     </div>
+        <Navbar/>
+        <div className='mx-[50px] my-[50px] flex items-center'>
         
-    //     <div className='w-6/12'>
-    //         <div className='my-6'>
-    //             <h1 className='text-xl font-bold'>{product.title}</h1>
-    //             <h6 className=' text-slate-400'>{product.category}</h6>
-    //         </div>
+            <div className='flex justify-center items-center w-6/12'>
+                <img className=' h-[500px]'  src={product.image} alt="product image" />
+            </div>
             
-    //         <h4 className='font-light'>{product.description}</h4>
-    //         <div className='mt-10'>
-    //             <button className='py-2 px-6 bg-blue-600 rounded-md text-white'>buy</button>
-    //         </div>
+            <div className='w-6/12'>
+                <div className='my-6'>
+                    <h1 className='text-xl font-bold'>{product.title}</h1>
+                    <h6 className=' text-slate-400'>{product.category}</h6>
+                </div>
+                
+                <h4 className='font-light'>{product.description}</h4>
+                <div className='mt-10'>
+                    <button className='py-2 px-6 bg-blue-600 rounded-md text-white'>buy</button>
+                </div>
 
-    //     </div>
-    // </div>
+            </div>
+        </div>
+    </div>
+    
   )
 }
--- app/product/page.js
+++ app/product/page.js
@@ -2,7 +2,7 @@ import React from 'react'
 import Card from "../components/Card";
 import Link from 'next/link';
 export default async function page() {
-const getData = await fetch('https://fakestoreapi.com/products');
+    const getData = await fetch('https://fakestoreapi.com/products');
   const products = await getData.json();
   return (
     <div className='mx-[50px]'>
@@ -12,6 +12,7 @@ const getData = await fetch('https://fakestoreapi.com/products');
             return <div key={product.id}>
                 <Link href={`product/${product.id}`}>
                 <Card 
+                    
                     title={product.title} 
                     image={product.image} 
                     price={product.price} 
